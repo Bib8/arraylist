46d739ba6c1cd7ab7463930dc9c014f81ce4e8ae
pro100 <frostymag177@gmail.com>
2022-02-12 16:06:55 +0300

    add new branch

--- src/main/java/ashdihomwork252arraylist/NotFoundAnyMatchException.java
+++ src/main/java/ashdihomwork252arraylist/NotFoundAnyMatchException.java
@@ -5,6 +5,22 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "Employee Not Found")
 public class NotFoundAnyMatchException extends Exception{
 
+    public NotFoundAnyMatchException() {
+    }
+
     public NotFoundAnyMatchException(String s) {
+        super(s);
+    }
+
+    public NotFoundAnyMatchException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public NotFoundAnyMatchException(Throwable cause) {
+        super(cause);
+    }
+
+    public NotFoundAnyMatchException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
     }
 }
