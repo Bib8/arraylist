8c98f04e9b4551c842272715c24735af0609975b
pro100 <frostymag177@gmail.com>
2022-02-12 14:43:20 +0300

    add map servvice and mapInterface

---
+++ src/main/java/ashdihomwork252arraylist/NotFoundAnyMatchException.java
@@ -0,0 +1,10 @@
+package ashdihomwork252arraylist;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+@ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "Employee Not Found")
+public class NotFoundAnyMatchException extends Exception{
+
+    public NotFoundAnyMatchException(String s) {
+    }
+}
